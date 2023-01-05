9dab1d611e84ad739e354ca51d745228485c9657
J. Waananen <j.waananen@protonmail.com>
2023-01-05 13:19:12 +0200

    POST method implementation @FE

--- src/components/NewCard.tsx
+++ src/components/NewCard.tsx
@@ -1,15 +1,32 @@
-import Input from "@mui/material/Input";
+import { useState } from "react";
+import axios from "axios";
 
+import Input from "@mui/material/Input";
 import CheckIcon from "@mui/icons-material/Check";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 const NewCard = () => {
-    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-        console.log("click", event);
-    };
+    const [inputText, setInputText] = useState({ task: "" });
 
     const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value);
+        const { name, value } = event.target;
+        setInputText({ ...inputText, [name]: value });
+    };
+
+    const handleClick = async (event: React.MouseEvent<HTMLDivElement>) => {
+        console.log("click", event);
+        try {
+            if (inputText.task.length < 1) {
+                throw new Error();
+            }
+            await axios.post("http://localhost:8080/tasks", {
+                task: inputText.task,
+            });
+            console.log("POST ok");
+            setInputText({ task: "" });
+        } catch (error) {
+            console.log("Catch block:", error);
+        }
     };
 
     return (
@@ -19,6 +36,7 @@ const NewCard = () => {
                     <Input
                         autoFocus
                         type="text"
+                        name="task"
                         placeholder="What ya wanna do?"
                         onChange={handleInput}
                     />
